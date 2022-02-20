d15803ba556e6b6287eaea30101fa215691c70ec
noelfranthomas <noelfranthomas@gmail.com>
2022-02-20 09:49:27 -0700

    transaction success page

--- pages/profile.js
+++ pages/profile.js
@@ -6,36 +6,11 @@ import styles from '../styles/Profile.module.css'
 import uploadcare from 'uploadcare-widget'
 
 export default function Profile() {
-
-  // const [windowSize, setWindowSize] = useState({
-  //   width: undefined,
-  //   height: undefined,
-  // });
-
-  // useEffect(() => {
-  //   // only execute all the code below in client side
-  //   if (typeof window !== 'undefined') {
-  //     // Handler to call on window resize
-  //     function handleResize() {
-  //       // Set window width/height to state
-  //       setWindowSize({
-  //         width: window.innerWidth,
-  //         height: window.innerHeight,
-  //       });
-  //     }
-    
-  //     // Add event listener
-  //     window.addEventListener("resize", handleResize);
-     
-  //     // Call handler right away so state gets updated with initial window size
-  //     handleResize();
-    
-  //     // Remove event listener on cleanup
-  //     return () => window.removeEventListener("resize", handleResize);
-  //   }
-  // }, []); // Empty array ensures that effect is only run on mount
   var file_src = "d0cfd8a6-ae78-42c3-97d0-ad3f1b49174b/noel.png";
-
+  var name = "Noel Thomas";
+  var pronouns = "He/Him";
+  var pos = "Developer";
+  var org = "uofc";
   return (
     <div>
       <Head>
@@ -51,16 +26,16 @@ export default function Profile() {
         </div>
         <br/>
         <div className='center'>
-          <h2>Noel Thomas</h2>
-          Pronouns
+          <h2>{name}</h2>
+          {pronouns}
 
           <br/>
 
-          <h4>Position @ Organization</h4>
+          <h4>{pos} @ {org}</h4>
           <div className={styles.back}>
           <a href="/user_home">
             <b>
-            Go back
+            Back to Home
             </b>
           </a>
         </div>
