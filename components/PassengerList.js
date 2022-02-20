c4b31662cf9d708b7ebe7109f6dbf48b3698b58b
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2022-02-19 22:42:14 -0700

    cleaned up some unnecessary files/folders

---
+++ components/PassengerList.js
@@ -0,0 +1,10 @@
+const PassengerList = () => {
+    return (
+        <div>
+            <h2>Here are your passengers:</h2>
+            
+        </div>
+    );
+}
+ 
+export default PassengerList;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout'
 import { PublicClientApplication } from '@azure/msal-browser';
 import { MsalProvider } from '@azure/msal-react';
 import { msalConfig } from '../src/authConfig';
@@ -8,11 +7,9 @@ function MyApp({ Component, pageProps }) {
   const msalInstance = new PublicClientApplication(msalConfig);
 
   return (
-    <Layout>
       <MsalProvider instance={msalInstance}>
         <Component {...pageProps} />
       </MsalProvider>
-    </Layout>
   )
 }
 
