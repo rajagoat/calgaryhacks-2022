1cb571e0915753902fc57cfc7e0cc32be332f2a8
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2022-02-19 17:23:56 -0700

    basic login page setup, needs proper auth still though

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,13 @@
 const Login = () => {
     return (
         <div>
-            
+            <h1>Log in</h1>
+            <form>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email"/>
+                <label htmlFor="password">Password</label>
+                <input type="text" id="password" name="password"/>
+            </form>
         </div>
     );
 }
