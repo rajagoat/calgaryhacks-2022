80e967a52bef4a83d5fd62cbff832ef817560e15
Athul Rajagopal <49791750+rajagoat@users.noreply.github.com>
2022-02-20 10:26:26 -0700

    Update server.js

--- payment/server.js
+++ payment/server.js
@@ -1,17 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
-
-const cors = require('cors');
-
-
 const app = express();
+const cors = require('cors');
+app.use(express.json());
 app.use(
     cors({
         origin: "http://localhost:3001",
     })
 )
-app.use(express.json());
 
 app.listen(3000);
 
